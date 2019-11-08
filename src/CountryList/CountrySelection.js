fc92854a18440713817c4030218612c0ba732746
Dexwin LTD <dexwinltd@gmail.com>
2019-11-08 20:16:38 +0000

    custom text style

--- src/CountryList/CountrySelection.js
+++ src/CountryList/CountrySelection.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React from "react";
 import {
-  View, TextInput, Image, SectionList, Text, TouchableOpacity,
-} from 'react-native';
-import styles from './countrySelectionStyles';
-import { searchIcon, countrySelectionTick, countries } from './Constants';
+  View,
+  TextInput,
+  Image,
+  SectionList,
+  Text,
+  TouchableOpacity
+} from "react-native";
+import styles from "./countrySelectionStyles";
+import { searchIcon, countrySelectionTick, countries } from "./Constants";
 
 /**
  * Item view
  * @param {*} params
  */
-const ItemView = (params) => {  
+const ItemView = params => {
   let text = `${params.item.name} (+${params.item.callingCode})`;
   let selected = null;
-  if (params.selected != null && params.selected.callingCode === params.item.callingCode) {
-    selected = <Image source={countrySelectionTick} style={styles.selectionTick} />;
+  if (
+    params.selected != null &&
+    params.selected.callingCode === params.item.callingCode
+  ) {
+    selected = (
+      <Image source={countrySelectionTick} style={styles.selectionTick} />
+    );
   }
   return (
     <View style={styles.itemContainer}>
-      <TouchableOpacity style={styles.itemTextContainer} onPress={() => params.action(params.item)}>
+      <TouchableOpacity
+        style={styles.itemTextContainer}
+        onPress={() => params.action(params.item)}
+      >
         <Image source={{ uri: params.item.flag }} style={styles.flag} />
-        <Text numberOfLines={1} style={styles.itemText}>{text}</Text>
-        <View style={styles.selectionView}>
-          { selected }
-        </View>
+        <Text
+          numberOfLines={1}
+          style={[styles.itemText, params.itemTextCustom]}
+        >
+          {text}
+        </Text>
+        <View style={styles.selectionView}>{selected}</View>
       </TouchableOpacity>
       <View style={styles.itemSeparator} />
     </View>
@@ -46,7 +62,7 @@ export default class CountrySelection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sections: [],
+      sections: []
     };
   }
 
@@ -57,21 +73,25 @@ export default class CountrySelection extends React.Component {
   /**
    * Change search text action
    */
-  onChangeSearchText = (text) => {
-    const filtered = countries.filter(country => country.name.toLowerCase().indexOf(text.toLowerCase()) > -1);
+  onChangeSearchText = text => {
+    const filtered = countries.filter(
+      country => country.name.toLowerCase().indexOf(text.toLowerCase()) > -1
+    );
     this.generateSectionData(filtered);
-  }
+  };
 
   /**
    * Generate section data from country list
    */
   generateSectionData(countryList) {
-    const sections = [];    
+    const sections = [];
     const sectionHeaders = countryList.map(data => data.name.charAt(0));
     const uniqueHeaders = Array.from(new Set(sectionHeaders));
 
-    uniqueHeaders.forEach((item) => {
-      const filtered = countryList.filter(country => country.name.charAt(0) === item);
+    uniqueHeaders.forEach(item => {
+      const filtered = countryList.filter(
+        country => country.name.charAt(0) === item
+      );
       sections.push({ title: item, data: filtered });
     });
     this.setState({ sections });
@@ -88,7 +108,7 @@ export default class CountrySelection extends React.Component {
             <Image source={searchIcon} style={styles.searchIcon} />
             <TextInput
               style={styles.textInput}
-              placeholder= 'Search'
+              placeholder="Search"
               placeholderTextColor="#2d2926"
               enablesReturnKeyAutomatically
               clearButtonMode="while-editing"
@@ -102,11 +122,14 @@ export default class CountrySelection extends React.Component {
               item={item}
               index={index}
               section={section}
-              action={(item) => action(item)}
+              action={item => action(item)}
               selected={selected}
+              itemTextCustom={this.props.itemTextCustom}
             />
           )}
-          renderSectionHeader={({ section: { title } }) => (<SectionHeader title={title} />)}
+          renderSectionHeader={({ section: { title } }) => (
+            <SectionHeader title={title} />
+          )}
           sections={sections}
           keyExtractor={(item, index) => item + index}
         />
